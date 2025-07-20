3ff0ff98e94640abb2eb957077568e1302a059d2
caveaku <85325018+caveaku@users.noreply.github.com>
2025-07-20 13:06:55 -0400

    Update app.ts

--- bin/app.ts
+++ bin/app.ts
@@ -1,1 +1,15 @@
+#!/usr/bin/env node
+import { App } from 'aws-cdk-lib';
+import { SecureBucket } from '../lib/secure-bucket';
 
+const app = new App();
+
+new SecureBucket(app, 'SecureBucketStack', {
+  projectId: 'abc123',
+  enableVersioning: true,
+  enableEncryption: true,
+  githubRepo: 'caveaku/sr-devops-challange',
+  env: {
+    region: 'us-east-1'
+  }
+});
