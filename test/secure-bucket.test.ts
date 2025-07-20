5901c72dd756d161c4f93dfa848f812b03e75cfc
caveaku <85325018+caveaku@users.noreply.github.com>
2025-07-20 13:08:02 -0400

    Update secure-bucket.test.ts

--- test/secure-bucket.test.ts
+++ test/secure-bucket.test.ts
@@ -1,1 +1,18 @@
+import { Template } from 'aws-cdk-lib/assertions';
+import { App } from 'aws-cdk-lib';
+import { SecureBucket } from '../lib/secure-bucket';
 
+test('S3 Bucket Created', () => {
+  const app = new App();
+
+  const stack = new SecureBucket(app, 'TestStack', {
+    projectId: 'abc123',
+    enableVersioning: true,
+    enableEncryption: true,
+    githubRepo: 'caveaku/sr-devops-challange'
+  });
+
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs('AWS::S3::Bucket', 1);
+});
