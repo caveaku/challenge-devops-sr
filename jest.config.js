9bef3b4d176333e4685525a7c4fbd173817c13c9
caveaku <85325018+caveaku@users.noreply.github.com>
2025-07-20 13:09:12 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,12 @@
-
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/test/**/*.test.ts'],
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest'
+    },
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    verbose: true
+  };
+  
